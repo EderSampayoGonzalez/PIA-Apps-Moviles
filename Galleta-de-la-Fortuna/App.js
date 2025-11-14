812b00c78b3b2f593c5d4d24035e2a7ac2b9af25
Eder Abraham Sampayo Gonzalez <eder.sampayog@uanl.edu.mx>
2025-11-14 17:53:55 -0600

    Agregadas funcionalidades
    Agregado:
    sonido - reproduce el sonido cuando se rompe la galleta
    
    funcionalidad de vida - al agitar el teléfono, la galleta va perdiendo vida hasta romperse

--- Galleta-de-la-Fortuna/App.js
+++ Galleta-de-la-Fortuna/App.js
@@ -1,13 +1,37 @@
-//import { DeviceMotion } from 'expo-sensors';
 import { Accelerometer } from 'expo-sensors';
 import { useState, useEffect } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { useAudioPlayer } from 'expo-audio';
+
+const cookieCrackSound = require('./assets/galleta1.mp3');
 
 export default function App() {
+  //Configuraciones del acelerómetro
   Accelerometer.setUpdateInterval(200);
-
   const [motionData, setMotionData] = useState(null);
+
+  // Configuraciones de la galleta de la fortuna
+  let [hp, setHp] = useState(30);
+  const [boxColor, setBoxColor] = useState('skyblue');
+  const audioPlayer = useAudioPlayer(cookieCrackSound);
+
+  const reproducirSonido = () => {
+    //no reproducir si el audioPlayer no está listo o está reproduciendo
+    if (!audioPlayer || audioPlayer.paused === false) {
+      return;
+    }
+
+    // velocidad de reproduccion aleatoria entre 0.6 y 1.6
+    const ratio = Math.random() * (1.6 - 0.6) + 0.6; 
+
+    audioPlayer.shouldCorrectPitch = false;
+    audioPlayer.setPlaybackRate(ratio);
+
+    audioPlayer.seekTo(0);
+    audioPlayer.play();
+  }
+
 
   const requestPermissionAndStartListening = async () => {
     if (motionData) {
@@ -16,66 +40,88 @@ export default function App() {
     }
 
     // Obtener permisos
-    
-    //const { status } = await DeviceMotion.getPermissionsAsync();
     const { status } = await Accelerometer.getPermissionsAsync();
     console.log('Permission status:', status);
 
     // Solicitar permisos si no están concedidos
     if (status !== 'granted') {
-      //const { status: newStatus } = await DeviceMotion.requestPermissionsAsync();
       const { status: newStatus } = await Accelerometer.requestPermissionsAsync();
       console.log('New permission status:', newStatus);
     }
 
-    // Verificar disponibilidad del sensor
-    //const available = await DeviceMotion.isAvailableAsync();
+    // Verificar disponibilidad del sensor);
     const available = await Accelerometer.isAvailableAsync();
     console.log('Accelerometer available:', available);
     
     // Iniciar escucha de datos del sensor
+    /*
+      Acélerómetro: detecta movimientos en los ejes.
+      Al detectar un movimiento, baja la vida de la galleta.
+      La vida baja más si el movimiento es más fuerte.
+    */
     if (available) {
       Accelerometer.addListener((data) => {
         //console.log(data);
-        let x = data.x;
-        let y = data.y - 1; // Ajustar para gravedad
-        let z = data.z;
+        const x = data.x;
+        const y = data.y - 1; // Ajustar para gravedad
+        const z = data.z;
 
-        /*if (x > 1){
-          console.log("Movimiento fuerte a la derecha");
+        let damage = 0;
+
+        // ignorar cuando el celular está en reposo y hacia abajo
+        if ((y >= 1.0 || y <= -1.0) && (x >= -0.3 && x <= 0.3)) {
+          //console.log("Celular en reposo, ignorando.");
+          return;
         }
-        if (x < -1){
-          console.log("Movimiento fuerte a la izquierda");
-        }
-        if (y > 1){
-          console.log("Movimiento fuerte hacia arriba");
-        }
-        if (y < -1){
-          console.log("Movimiento fuerte hacia abajo");
-        }
-        if (z > 1){
-          console.log("Movimiento fuerte hacia adelante");
-        }
-        if (z < -1){
-          console.log("Movimiento fuerte hacia atrás");
-        }*/
         
-        //Lógoica para detectar sacudidas (Solo hacia arriba/abajo)
-        if (data.y >= 1.2 || data.y <= -1.2) {
-          console.log("se agitó fuerte el celular");
+        //Lógica para detectar sacudidas
+        if (y >= 1.8 || y <= -1.8 || x >= 1.8 || x <= -1.8) {
+          //console.log("se agitó fuerte el celular con: x=" + x + " y=" + y);
+          setBoxColor('red');
+          reproducirSonido();
+          damage = 3;
         }
-        else if (data.y >= 1.0 || data.y <= -1.0) {
-          console.log("se agitó el celular");
+        else if (y >= 1.3 || y <= -1.3 || x >= 1.3 || x <= -1.3) {
+          //console.log("se agitó el celular con: x=" + x + " y=" + y);
+          setBoxColor('orange');
+          reproducirSonido();
+          damage = 1;
         }
+        else {
+          setBoxColor('skyblue');
+        }
+
+        setHp((prevHP) => prevHP - damage);
 
         setMotionData(data);
       });
     }
   };
 
+  if (hp <= 0) {
+    return (
+      <View style={[styles.container, {backgroundColor: 'black'}]}>
+        <Text style={{color: 'white', fontSize: 30, fontWeight: 'bold', textAlign: 'center'}}>
+          ¡La galleta de la fortuna se ha roto! {"\n"}
+        </Text>
+        <Button 
+          title="Reiniciar Galleta"
+          onPress={() => {
+            setHp(30);
+            setBoxColor('skyblue');
+          }}
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={[styles.container, {backgroundColor: boxColor}]}>
+      <Text>{hp} {"\n"}Prueba: Cuadrado que cambia de color al agitarse</Text>
+      <View style={[styles.caja, { backgroundColor: boxColor }]}>
+        <Text style={{ fontSize: 24, fontWeight: 'bold' }}> vida: {hp} </Text>
+      </View>
       <Button 
         title="Activar Acelerómetro" 
         onPress={requestPermissionAndStartListening}
@@ -100,5 +146,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  caja: {
+    width: 200,
+    height: 200,
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 20,
+  }
 });
 
